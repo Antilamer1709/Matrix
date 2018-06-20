5395d2cb50ef2b150190d7d790126672e16e6c42
vsloboda <vsloboda@ittouch.pl>
2018-06-20 13:01:46 +0200

    http interceptor

--- src/main/resources/front/src/app/app.module.ts
+++ src/main/resources/front/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthenticationModule} from "./authentication/authentication.module";
 import {RouterModule} from "@angular/router";
 import {routes} from "./app.routes";
 import {GrowlModule} from "primeng/growl";
 import {MessageService} from "primeng/components/common/messageservice";
+import {CustomHttpInterceptor} from "./common/http-interceptor";
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import {MessageService} from "primeng/components/common/messageservice";
     AuthenticationModule,
     GrowlModule
   ],
-  providers: [MessageService],
+  providers: [
+    MessageService,
+    {provide: HTTP_INTERCEPTORS, useClass: CustomHttpInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/main/resources/front/src/app/common/http-interceptor.ts
@@ -0,0 +1,53 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent, HttpResponse
+} from '@angular/common/http';
+
+import {Injectable, Injector} from "@angular/core";
+import {MessageService} from "primeng/components/common/messageservice";
+import {Observable} from "rxjs/internal/Observable";
+import {finalize, tap} from "rxjs/operators";
+
+@Injectable()
+export class CustomHttpInterceptor implements HttpInterceptor {
+  messageService: MessageService;
+
+  constructor(
+    private injector: Injector
+  ) {
+
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let ok: string;
+    let response;
+
+    if (!this.messageService)
+      this.messageService = this.injector.get(MessageService); // get it here within intercept
+
+    return next.handle(request)
+      .pipe(
+        tap(
+          // Succeeds when there is a response; ignore other events
+          event => ok = event instanceof HttpResponse ? 'succeeded' : '',
+          // Operation failed; error is an HttpErrorResponse
+          error => {
+            ok = 'failed'
+            response = error;
+          }
+        ),
+        // Log when response observable either completes or errors
+        finalize(() => {
+          if (ok === 'failed') {
+            if (response.status === 401) {
+              this.messageService.add({severity: 'error', summary: 'Error', detail: 'Authentication failed!'});
+            } else {
+              this.messageService.add({severity: 'error', summary: 'Error', detail: 'Something happaned!'});
+            }
+          }
+        })
+      );
+  }
+}
