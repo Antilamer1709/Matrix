fc0c12780e6464b20879236f057846d3340dce75
vsloboda <vsloboda@ittouch.pl>
2018-06-21 17:28:32 +0200

    topic card

--- src/main/resources/front/src/app/topic/topic-card/topic-card.component.ts
+++ src/main/resources/front/src/app/topic/topic-card/topic-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {TopicDTO} from "../topic-model";
 
 @Component({
   selector: 'app-topic-card',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topic-card.component.css']
 })
 export class TopicCardComponent implements OnInit {
+
+  @Input('topic')
+  topic: TopicDTO;
 
   constructor() { }
 
