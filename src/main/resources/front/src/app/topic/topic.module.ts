067de69c2045bc8a40804afef276fbe38a8a3a12
vsloboda <vsloboda@ittouch.pl>
2018-06-21 19:44:20 +0200

    new topic container

--- src/main/resources/front/src/app/topic/topic.module.ts
+++ src/main/resources/front/src/app/topic/topic.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TopicListComponent } from './topic-list/topic-list.component';
 import { TopicCardComponent } from './topic-list/topic-card/topic-card.component';
 import { TopicComponent } from './topic/topic.component';
+import { NewTopicComponent } from './new-topic/new-topic.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TopicListComponent, TopicCardComponent, TopicComponent]
+  declarations: [TopicListComponent, TopicCardComponent, TopicComponent, NewTopicComponent]
 })
 export class TopicModule { }
--- src/main/resources/front/src/app/topic/topic.routes.ts
+++ src/main/resources/front/src/app/topic/topic.routes.ts
@@ -1,8 +1,10 @@
 import {Route} from "@angular/router";
 import {TopicListComponent} from "./topic-list/topic-list.component";
 import {TopicComponent} from "./topic/topic.component";
+import {NewTopicComponent} from "./new-topic/new-topic.component";
 
 export const TopicRoutes: Route[] = [
   { path: 'topic', component: TopicListComponent },
+  { path: 'topic/new', component: NewTopicComponent },
   { path: 'topic/:id', component: TopicComponent }
 ];
