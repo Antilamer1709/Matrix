d57a3615e6515df8fe8ec6f8a836f13dc930a322
vsloboda <vsloboda@ittouch.pl>
2018-06-25 18:30:19 +0200

    New evidence: init topic

--- src/main/resources/front/src/app/topic/evidence/evidence.component.ts
+++ src/main/resources/front/src/app/topic/evidence/evidence.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup} from "@angular/forms";
 import {AuthenticationService} from "../../authentication/authentication.service";
+import {ActivatedRoute, Params} from "@angular/router";
+import {TopicService} from "../topic.service";
+import {EvidenceService} from "../topic/evidence.service";
+import {TopicDTO} from "../topic-model";
 
 @Component({
   selector: 'app-evidence',
@@ -11,9 +15,29 @@ export class EvidenceComponent implements OnInit {
 
   comment: string = "";
 
-  constructor(public authenticationService: AuthenticationService) { }
+  topicId: number;
+  topic: TopicDTO = new TopicDTO();
+
+  constructor(public authenticationService: AuthenticationService,
+              private topicService: TopicService,
+              private evidenceService: EvidenceService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.initTopicId();
+    this.initTopic();
+  }
+
+  private initTopicId(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.topicId = params['id'];
+    });
+  }
+
+  private initTopic(): void {
+    this.topicService.getTopic(this.topicId).subscribe( res => {
+      this.topic = res;
+    })
   }
 
   public addComment(form: FormGroup): void {
