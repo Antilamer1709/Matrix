3ffa3815a2ec2471b0162cf9e86d90323cc328f7
vsloboda <vsloboda@ittouch.pl>
2018-07-02 18:17:23 +0200

    lombok log config

--- src/main/java/com/innovationPassport/matrix/controller/AuthenticationController.java
+++ src/main/java/com/innovationPassport/matrix/controller/AuthenticationController.java
@@ -4,10 +4,12 @@ import com.innovationPassport.matrix.dto.RegistrationDTO;
 import com.innovationPassport.matrix.dto.UserDTO;
 import com.innovationPassport.matrix.exception.ValidationException;
 import com.innovationPassport.matrix.service.AuthenticationBO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/authentication")
 public class AuthenticationController {
@@ -18,11 +20,13 @@ public class AuthenticationController {
     @PostMapping(value = "/registration")
     @ResponseStatus(value = HttpStatus.OK)
     public void registration(@RequestBody RegistrationDTO registrationDTO) throws ValidationException {
+        log.debug("*** registration() registrationDTO: " + registrationDTO);
         authenticationBO.registerUser(registrationDTO);
     }
 
     @PostMapping(value = "/loggedUser")
     public UserDTO loggedUser() {
+        log.debug("*** loggedUser()");
         return authenticationBO.getLoggedUserDTO();
     }
 
