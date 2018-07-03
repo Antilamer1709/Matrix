4e6bca7d7df0f05baf2d70530779495cad0849cd
vsloboda <vsloboda@ittouch.pl>
2018-07-03 19:24:34 +0200

    user profile (front)

--- src/main/resources/front/src/app/user-profile/user-profile.module.ts
+++ src/main/resources/front/src/app/user-profile/user-profile.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import {TableModule} from "primeng/table";
+import {DropdownModule} from "primeng/primeng";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TableModule,
+    DropdownModule
   ],
   declarations: [UserProfileComponent]
 })
--- src/main/resources/front/src/app/user-profile/user-profile/user-profile.component.ts
+++ src/main/resources/front/src/app/user-profile/user-profile/user-profile.component.ts
@@ -3,23 +3,34 @@ import {UserDTO} from "../../authentication/authentication-model";
 import {MessageService} from "primeng/components/common/messageservice";
 import {UserProfileService} from "../user-profile.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {CommonComponent} from "../../common/common-component";
+import {EvidenceDTO} from "../../topic/topic-model";
+import {LazyLoadEvent} from "primeng/api";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent extends CommonComponent implements OnInit {
+
+  loading: boolean = false;
+  totalRecords: number;
+  cols: any[];
+  data: EvidenceDTO[];
 
   public userId: number;
   public user: UserDTO = new UserDTO();
 
   constructor(private messageService: MessageService,
               private userProfileService: UserProfileService,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute) {
+    super();
+  }
 
   ngOnInit() {
     this.initRouteParams();
+    this.initColumns();
   }
 
   private initRouteParams(): void {
@@ -34,6 +45,29 @@ export class UserProfileComponent implements OnInit {
     this.userProfileService.getUser(this.userId).subscribe(res => {
       this.user = res;
     });
+  }
+
+  public loadEvidences(event: LazyLoadEvent) {
+    setTimeout(() => {
+      this.loading = true;
+    });
+    // this.evidenceService.getEvidences(this.topicId, event).subscribe(x => {
+    //   this.loading = false;
+    //   this.totalRecords = x.totalElements;
+    //   this.data = x.data;
+    // });
+  }
+
+  public onRowSelect(event): void {
+    // this.router.navigate(['topic/' + this.topicId + '/evidence/' + event.data.id]);
+  }
+
+  private initColumns(): void {
+    this.cols = [
+      { field: 'evidence', header: 'Evidence' },
+      { field: 'source', header: 'Source' },
+      { field: 'credibility', header: 'Credibility' }
+    ];
   }
 
 }
